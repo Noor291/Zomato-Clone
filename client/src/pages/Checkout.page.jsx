7679c12e4850b1a3c547205439866809ccf8f35f
Noor <noordeep29oct@gmail.com>
2023-06-27 20:05:52 +0530

    changes

--- client/src/pages/Checkout.page.jsx
+++ client/src/pages/Checkout.page.jsx
@@ -78,7 +78,7 @@ const Checkout = () => {
           <h5 className="text-base tracking-wider">ORDER FROM</h5>
           <div className="flex w-full flex-col items-center text-gray-400">
             <h4>Domino's Pizza</h4>
-            <small>GT World Mall, Magadi Road, NCR Noida</small>
+            <small>GT World Mall, Tanda</small>
           </div>
           <div className="my-4 h-32 overflow-y-scroll px-4 flex flex-col gap-2 w-full md:w-3/5">
             {cart.map((item) => (
