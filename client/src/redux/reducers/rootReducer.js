5ed51ce85c30f1303251f7d2be7a8fdfc0909afe
Noor <noordeep29oct@gmail.com>
2023-06-28 23:35:54 +0530

    signin

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 
 // reducers or storage units
 import auth from "./auth/auth.reducer";
-import userReducer from "./user/user.reducer";
+import user from "./user/user.reducer";
+
 const rootReducer = combineReducers({
-  auth,userReducer
+  auth,
+  user,
 });
 
 export default rootReducer;
